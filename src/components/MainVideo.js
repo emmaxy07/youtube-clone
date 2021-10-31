26baddac37d8de7a61954d35e369270700d6f19a
emmanuel <emmanueliyare7@gmail.com>
2021-10-31 01:55:23 +0100

    main video updated

---
+++ src/components/MainVideo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MainVideo =()=>{
+    return(
+      <div>
+        <div>
+        <iframe width="560" height="560"
+         src="https://www.youtube.com/embed/8DYlzVUTC7s"
+         title="YouTube video player" frameborder="0"
+         allow="accelerometer; autoplay; clipboard-write;
+         encrypted-media; gyroscope; picture-in-picture"
+         allowfullscreen>
+         </iframe>
+        </div>
+      </div>
+    )
+}
+
+export default MainVideo
